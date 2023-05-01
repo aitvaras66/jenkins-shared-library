9e796b1f62f1b27b15d757f6ec6c899c6cf1c2ba
Aivaras S <aivaras.stabingis@gmail.com>
2023-05-01 20:19:54 +0300

    Update Jenkinsfile

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -4,3 +4,8 @@ def call() {
     echo "building the application for branch BRANCH_NAME"
     sh 'mvn package'
 }
+//mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
+// triple / when in groovy scripts/Jenkinsfile like \\\${}
+// def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+// def version = matcher[0][1]
+// env.IMAGE_NAME = "$version-$BUILD_NUMBER"
